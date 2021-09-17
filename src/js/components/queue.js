6bfb3beb183ea035a12ce7189a3e4d3a012838c7
Maxim Ostrovsky <maxim.ostrovskiy86@gmail.com>
2021-09-17 20:16:33 +0300

    working localSorage

--- src/js/components/queue.js
+++ src/js/components/queue.js
@@ -1,10 +1,9 @@
 import newApiService from "../services/apiSevise";
-import {outputRefs, queueBtn, watchedBtn} from "../const/refs";
+import {outputRefs, watchedBtn, watchedBtnRefs} from "../const/refs";
 import localStorageFn from "./localStorage";
 import watchedQueueTpl from "../../templates/item-media.hbs";
 
-export const watchedSave = () => {
-  console.log(111)
+export const watchedSave = (e) => {
   const film = newApiService.openedFilm;
   const localWatched = localStorageFn.load('dataWatched');
   console.log(localWatched)
@@ -16,25 +15,44 @@ export const watchedSave = () => {
   }
 
   const isFind = localWatched.some(item => item.id === film.id);
+  const target = e.target;
 
-  // const checker = () => {
-  //   for (let i = 0; i < localWatched.length; i++) {
-  //     console.log(localWatched[i].id)
-  //     if (localWatched[i].id === film.id) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // };
+  const themeColor = {
+    enabled: 'Add to Watched',
+    disabled: 'Remove movie',
+  };
 
-  if (localWatched.length === 0 || !isFind) {
-    localWatched.push(film);
-    localStorageFn.save('dataWatched', localWatched);
+
+  function buttonSwitcher() {
+    const buttonStatus = localStorage.getItem('theme');
+    if (buttonStatus && buttonStatus === themeColor.disabled) {
+      target.classList.add('modal__btn-active');
+      target.textContent = 'Remove movie';
+      return;
+    }
+  }
+  // buttonSwitcher();
+
+
+  if (isFind) {
+    target.classList.remove('modal__btn-active');
+
+    const newLocalWatched = localWatched.filter(item => item.id !== film.id);
+
+    localStorage.setItem('theme', themeColor.enabled);
+    localStorageFn.save('dataWatched', newLocalWatched);
+    target.textContent = 'Add to Watched';
     return;
   }
 
-  // localWatched.push(film);
-
+  if (localWatched.length === 0 || !isFind) {
+    target.classList.add('modal__btn-active');
+    localWatched.push(film);
+    localStorageFn.save('dataWatched', localWatched);
+    target.textContent = 'Remove movie';
+    localStorage.setItem('theme', themeColor.disabled);
+    return;
+  }
 }
 
 export function appendWatchedMarkup() {
@@ -43,24 +61,24 @@ export function appendWatchedMarkup() {
   return outputRefs.innerHTML = watchedQueueTpl(localWatched);
 }
 
-// export default appendQueueMarkup;
-
-
 export const queueSave = () => {
   const film = newApiService.openedFilm;
   const localQueue = localStorageFn.load('dataQueue');
 
-  // console.log(localQueue)
+
   if (!localQueue) {
     const arrayQueue = [film];
     localStorageFn.save('dataQueue', arrayQueue);
     return;
   }
 
+  const isFind = localQueue.some(item => item.id === film.id);
 
-  localQueue.push(film)
-  localStorageFn.save('dataQueue', localQueue);
-  return localQueue;
+  if (localQueue.length === 0 || !isFind) {
+    localQueue.push(film);
+    localStorageFn.save('dataQueue', localQueue);
+    return;
+  }
 }
 
 export function appendQueueMarkup() {
